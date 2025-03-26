b5a63a8f32fe0a880074239528fdd0137c9ad3d6
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-26 19:30:13 +0530

    React Query Tutorial - 16 - Dynamic Parallel Queries

--- src/app/rq-pararllelqueries/page.js
+++ src/app/rq-pararllelqueries/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useQuery } from "react-query";
+import { useQuery, useQueries } from "react-query";
 import axios from "axios";
 const RqParallelQueries = () => {
   const fetchSuperHeroes = () => {
@@ -27,8 +27,26 @@ const RqParallelQueries = () => {
       {friends?.data?.map((friend) => {
         return <div key={friend.id}>{friend.name}</div>;
       })}
+      <hr />
+      <DynamicParallelComponent heroIds={[1, 3]} />
     </div>
   );
 };
 
 export default RqParallelQueries;
+
+const DynamicParallelComponent = ({ heroIds }) => {
+  const fetchSuperHero = (heroId) => {
+    return axios.get(`http://localhost:4000/superheroes/${heroId}`);
+  };
+
+  const queryResults = useQueries(
+    heroIds.map((id) => {
+      return { queryKey: ["superhero", id], queryFn: () => fetchSuperHero(id) };
+    })
+  );
+
+  console.log({queryResults});
+
+  return <div>Hello</div>;
+};
